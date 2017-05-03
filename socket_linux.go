0fcc669661516d52dfdf8ef121e80dc88dd45587
Kevin King <4kevinking@gmail.com>
2017-05-03 13:54:43 -0700

    log that kr is being restarted

--- socket_linux.go
+++ socket_linux.go
@@ -10,14 +10,14 @@ import (
 
 func DaemonDial(unixFile string) (conn net.Conn, err error) {
 	if runningErr := exec.Command("pgrep", "krd").Run(); runningErr != nil {
-		os.Stderr.WriteString("restarting krd\n");
+		os.Stderr.WriteString(Yellow("Kryptonite ▶ Restarting krd...\r\n"))
 		exec.Command("nohup", "/usr/bin/krd", "&").Start()
 		<-time.After(time.Second)
 	}
 	conn, err = net.Dial("unix", unixFile)
 	if err != nil {
 		//	restart then try again
-		os.Stderr.WriteString("restarting krd\n");
+		os.Stderr.WriteString(Yellow("Kryptonite ▶ Restarting krd...\r\n"))
 		exec.Command("killall", "krd").Start()
 		exec.Command("nohup", "/usr/bin/krd", "&").Run()
 		<-time.After(time.Second)
